768a95352c1d7ccc51b9076982d92b9d5d86ea22
sreblov <code@sreblov.de>
2021-11-05 21:46:43 +0100

    implemented example action (ls of current dir)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use log::{error};
 use serde_derive::Deserialize;
-use notify::{RecommendedWatcher, Watcher, RecursiveMode};
+use notify::{RecommendedWatcher, Watcher, RecursiveMode, DebouncedEvent};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 use std::{env, fs};
+use std::io::{self, Write};
+use std::process::Command;
 
 const CONFIG_FILE: &'static str = "scanwatch.toml";
 
@@ -22,6 +24,17 @@ fn watch(config: &Config) -> notify::Result<()> {
 
     loop {
         match rx.recv() {
+            Ok(DebouncedEvent::Write(pathbuf)) => {
+                println!("Write to {}", pathbuf.to_string_lossy());
+                let output =
+                    Command::new("ls")
+                    //.arg("ls")
+                    .output()
+                    .expect("failed to execute process");
+
+                io::stdout().write_all(&output.stdout).unwrap();
+                println!("---");
+            }
             Ok(event) => println!("{:?}", event),
             Err(e) => error!("watch error: {:?}", e),
         }
