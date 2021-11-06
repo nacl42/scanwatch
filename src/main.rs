7a05efb861b51a8315b864b2aea07e10e65d84b8
sreblov <code@sreblov.de>
2021-11-06 22:38:46 +0100

    implemented desktop notifications; only forward pdfs to printer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+///! Very simple app that watches a directory for newly created
+///! or overwritten files and sends the file to a printer
+
+// TODO:
+// - only send pdf files
+// - read configuration from xdg-compatible directory
+// -
+
 use log::{error};
 use serde_derive::{Deserialize, Serialize};
+
 use notify::{RecommendedWatcher, Watcher, RecursiveMode, DebouncedEvent};
+
+use notify_rust::Notification;
+
 use std::sync::mpsc::channel;
 use std::time::Duration;
 use std::{env, fs};
@@ -8,6 +20,20 @@ use std::io::{self, Write};
 use std::process::Command;
 
 const CONFIG_FILE: &'static str = "scanwatch.toml";
+
+fn notify(message: &'_ str) {
+    // if below notification fails, then this is not at all fatal, as
+    // we print out the message on the command line
+    println!("{}", message);
+
+    Notification::new()
+        .summary("scanwatch")
+        .body(message)
+        .icon("printer")
+        .show()
+        .unwrap();
+}
+
 
 #[derive(Serialize, Deserialize)]
 struct Config {
@@ -26,15 +52,23 @@ fn watch(config: &Config) -> notify::Result<()> {
     loop {
         match rx.recv() {
             Ok(DebouncedEvent::Write(pathbuf)) |
-            Ok(DebouncedEvent::Create(pathbuf))|
-            Ok(DebouncedEvent::NoticeWrite(pathbuf)) => {
-                println!("» printing document '{}' to printer '{}'", pathbuf.to_string_lossy(), config.printer);
-                let _child =
-                    Command::new("lpr")
-                    .args([format!("-P{}", config.printer),
-                           format!("{}", pathbuf.to_string_lossy())])
-                    .spawn()
-                    .expect("failed to execute process");
+            Ok(DebouncedEvent::Create(pathbuf)) => {
+                // TODO: could be easily written using the nightly-feature `contains`
+                // let is_pdf = pathbuf.as_path().extension().contains("pdf");
+                match pathbuf.as_path().extension() {
+                    Some(ext) if ext == "pdf" => {
+                        let short_name = pathbuf.as_path().file_name().unwrap().to_string_lossy();
+                        notify(&format!("sending document '{}' to printer '{}'", short_name, config.printer));
+                        
+                        let _child =
+                            Command::new("lpr")
+                            .args([format!("-P{}", config.printer),
+                                   format!("{}", pathbuf.to_string_lossy())])
+                            .spawn()
+                            .expect("failed to execute process");
+                    }
+                    _ => { println!("skipped, because it is not a pdf file")}
+                };
             }
             Ok(event) => println!("unspecified event: {:?}", event),
             Err(e) => error!("watch error: {:?}", e),
@@ -53,8 +87,9 @@ fn main() {
     
     let config: Config = toml::from_str(&config_string).unwrap();
 
-    println!("Watching path {path}", path=config.path);
-
+    notify(&format!("Watching path {path} for incoming documents (pdf only)",
+                    path=config.path));
+        
     if let Err(e) = watch(&config) {
         error!("error: {:?}", e);
     }
